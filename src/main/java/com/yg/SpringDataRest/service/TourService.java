00f72b087f22a09aa46ae8fa4dbd415686875eaa
Yash Gupta <yashhguptaa1@gmail.com>
2021-12-08 17:42:12 +0530

    injecting using services

---
+++ src/main/java/com/yg/SpringDataRest/service/TourService.java
@@ -0,0 +1,58 @@
+package com.yg.SpringDataRest.service;
+
+import com.yg.SpringDataRest.domain.Difficulty;
+import com.yg.SpringDataRest.domain.Region;
+import com.yg.SpringDataRest.domain.Tour;
+import com.yg.SpringDataRest.domain.TourPackage;
+import com.yg.SpringDataRest.repo.TourPackageRepository;
+import com.yg.SpringDataRest.repo.TourRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TourService {
+    private TourRepository tourRepository;
+    private TourPackageRepository tourPackageRepository;
+
+    @Autowired
+    public TourService(TourRepository tourRepository, TourPackageRepository tourPackageRepository) {
+        this.tourRepository = tourRepository;
+        this.tourPackageRepository = tourPackageRepository;
+    }
+
+    /**
+     * Create a new Tour Object and persist it to the Database.
+     *
+     * @param title
+     * @param description
+     * @param blurb
+     * @param price
+     * @param duration
+     * @param bullets
+     * @param keywords
+     * @param tourPackageName
+     * @param difficulty
+     * @param region
+     * @return Tour Entity
+     */
+    public Tour createTour(String title, String description, String blurb, Integer price,
+                           String duration, String bullets,
+                           String keywords, String tourPackageName, Difficulty difficulty, Region region ) {
+        TourPackage tourPackage = tourPackageRepository.findById(tourPackageName).orElseThrow(()->
+                new RuntimeException("Tour package does not exist: " + tourPackageName));
+
+        return tourRepository.save(new Tour(title, description,blurb, price, duration,
+                bullets, keywords, tourPackage, difficulty, region));
+    }
+
+    /**
+     * Calculate the number of Tours in the Database.
+     *
+     * @return the total.
+     */
+    public long total() {
+        return tourRepository.count();
+    }
+
+}
+
