5431180ab0e917da873bf17f62a1fe9f2291e634
Yash Gupta <yashhguptaa1@gmail.com>
2021-12-08 14:39:42 +0530

    S3 Creating Demo or Bootstrap Loader

--- src/main/java/com/yg/SpringDataRest/model/Contact.java
+++ src/main/java/com/yg/SpringDataRest/model/Contact.java
@@ -1,4 +1,26 @@
 package com.yg.SpringDataRest.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class Contact {
+    @Id
+    @GeneratedValue
+    public Long id;
+    public String firstName;
+    public String lastName;
+    public String email;
+
+    public Contact() {}
+
+    public Contact(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
 }
+
